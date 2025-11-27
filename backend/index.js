202ce479c73d36bf03439829addb3a1e3e1035c0
Deepak Kumar Yadav <dky422003@gmail.comm>
2025-11-28 01:14:09 +0530

    created the database

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ const db = await mysql.createConnection({
     host: "localhost",
     user: "root",
     password: process.env.PASSWORD,
-    // database: "",
+    database: process.env.DATABASE,
 });
 
 console.log('MySQL connected successfully!');
+
+// await db.execute(`create database xeno`);
+
+// console.log(await db.execute("show databases"));
